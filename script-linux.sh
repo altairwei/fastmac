3351273c8f3f89567fd4a86d9432c4550b7d972a
Altair Wei <altair_wei@outlook.com>
2020-10-06 23:07:22 +0800

    Update script-linux.sh

--- script-linux.sh
+++ script-linux.sh
@@ -2,6 +2,11 @@ set -e -o pipefail
 shopt -s failglob
 export LC_ALL=C
 
+pip install conan --upgrade
+pip install conan_package_tools bincrafters_package_tools
+conan user
+conan remote add altairwei https://api.bintray.com/conan/altairwei/conan --insert
+
 if [[ "$TRAVIS_PYTHON_VERSION" == "2.7" ]]; then
   wget https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh -O miniconda.sh;
 else
@@ -16,13 +21,8 @@ conda info -a
 conda init bash
 source $HOME/.bashrc
 conda create -y -n build-v8 python=2.7
-
-pip install conan --upgrade
-pip install conan_package_tools bincrafters_package_tools
-conan user
-conan remote add altairwei https://api.bintray.com/conan/altairwei/conan --insert
-
 conda activate build-v8
+
 git clone https://github.com/altairwei/conan-v8.git
 cd conan-v8
 conan create . altairwei/testing
